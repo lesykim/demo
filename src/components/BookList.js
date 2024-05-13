89d192a9b24aa6aa484a87078643bc86f734672a
lesykim <trchol01@naver.com>
2024-05-13 18:49:14 +0900

    Add component

---
+++ src/components/BookList.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const BookList = () => {
+    // useState는 화면 랜더링에 반영됨
+    const [bookList, setBookList] = useState([]);
+    const [page, setPage] = useState(1);
+    const [search, setSearch] = useState('강아지똥');
+
+    // useRef는 화면 랜더링에 반영되지 않는 참조값
+    const pageCount = useRef(1);
+
+    const fetchBooks = async() => {
+        const response = await fetch(`https://dapi.kakao.com/v3/search/book?query=${search}&page=${page}`,
+            {
+                method: "GET",
+                headers: {
+                    Authorization : `KakaoAK ${process.env.REACT_APP_API_KEY}`,
+                },
+            }
+        );
+        const data = await response.json();
+        console.log(data);
+
+        pageCount.current = data.meta.pageable_count % 10 > 0 ? data.meta.pageable_count / 10 + 1 : data.meta.pageable_count / 10;
+        pageCount.current = Math.floor(pageCount.current);
+        pageCount.current =pageCount.current > 15 ? 15 : pageCount.current;
+
+        console.log(pageCount.current);
+        setBookList(data.documents);
+    }
+
+    useEffect(() => {
+        fetchBooks();
+    }, [page, search]);
+
+    const changeSearch = e => {
+        //내용 작성
+        if(e.target.value >= 2){
+            setSearch(e.target.value);
+        }
+    }
+
+    return (
+        <>
+            <h1>도서 검색 목록</h1>
+            <input type='text' placeholder='검색어 입력' onChange={changeSearch}></input>
+            <div>
+                {bookList.map(book => (
+                    <>
+                        <p>{book.title}</p>
+                    </>
+                ))}
+            </div>
+            <ul>
+                {
+                    Array.from({length: pageCount.current}, (_, index) => (
+                        <>
+                            <li onClick={e => {setPage(index + 1)}}>{index + 1}</li>
+                        </>
+                    ))
+                }
+            </ul>
+        </>
+    );
+};
+
+export default BookList;
